738b3a5b7c6b16fc1721c5517a1f8fd002c79756
senthilkmu01 <senthilkmu01@gmail.com>
2017-03-15 13:02:02 +0530

    jqwidget added

--- server.js
+++ server.js
@@ -17,6 +17,24 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/node_modules/jqwidgets-framework/jqwidgets/styles/jqx.base.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'node_modules/jqwidgets-framework/jqwidgets/styles', 'jqx.base.css'));
+});
+
+app.get('/node_modules/jqwidgets-framework/jqwidgets/styles/images/:imagefile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'node_modules/jqwidgets-framework/jqwidgets/styles/images',req.params.imagefile));
+});
+
+app.get('/scripts/:scriptfile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'node_modules/jqwidgets-framework/scripts',req.params.scriptfile));
+});
+
+
+app.get('/jqwidgets/:jqwidgetsfilename', function (req, res) {
+    res.sendFile(path.join(__dirname,'node_modules/jqwidgets-framework/jqwidgets',req.params.jqwidgetsfilename));
+});
+
+
 
 var port = 3000; // Use 8080 for local development because you might already have apache running on 80
 app.listen(3000, function () {
